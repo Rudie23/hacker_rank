c9cb20a175a114685389e07e5d8dfaf95ad0ce27
Diego <ruan.diego.anjos@outlook.com>
2023-08-20 01:26:23 -0300

    enumerate function

--- enuma.py
+++ enuma.py
@@ -1,9 +1,9 @@
-
 matrix = [
     [11, 2, 4],
     [4, 5, 6],
     [10, 8, -12]
 ]
 
-values = (row[i] for i, row in enumerate(matrix))
-next(values)
+values = (row[-i - 1] for i, row in enumerate(matrix))
+for value in values:
+    print(value)
