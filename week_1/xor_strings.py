461d9992792f9fec465b6c23df58526ed09b1b97
Diego <ruan.diego.anjos@outlook.com>
2023-10-15 01:58:11 -0300

    two arrays solved

--- week_1/xor_strings.py
+++ week_1/xor_strings.py
@@ -27,3 +27,19 @@ Sample Output
     10000
 
 """
+
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res = '0'
+        else:
+            res = '1'
+
+    return res
+
+
+s = input()
+t = input()
+print(strings_xor(s, t))
